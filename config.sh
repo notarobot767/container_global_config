2774ce945d4dbd641efb9f389c1c3ab267f55c9b
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-03-20 00:29:17 -0400

    added hardlink

--- config.sh
+++ config.sh
@@ -1,1 +1,32 @@
-/nfs/docker/config.sh
+#!/bin/bash
+
+NETWORK="SERVERS"
+#default docker network to use
+
+LOGDIR="/disk2/logs/chrony"
+#local dir for logs
+
+DNS="10.0.20.10"
+#local dns server container for network
+
+DNS_SEARCH="ogrydziak.net"
+#local dns domain search
+
+RESTART_MODE="on-failure"
+#docker restart mode
+
+TZ="America/New_York"
+#docker time zone
+
+PUID="1024"
+#docker UID
+
+PGID=$PUID
+#docker GID
+
+UMASK_SET="022"
+
+BINDING_IP="10.0.20.10"
+BINDing_IP2="10.0.20.9"
+
+PULL_MODE="always"
